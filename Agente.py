ec10a10cf9b156ea13ce622e73be792102e808da
Angel Ayala <angel4ayala@gmail.com>
2018-09-25 15:53:13 -0300

    Política epsilon-greedy

--- Agente.py
+++ Agente.py
@@ -6,7 +6,6 @@
 """
 import gym
 import math
-import random
 import numpy as np
 import matplotlib.pyplot as plt
 from gym.wrappers.monitor import load_results
@@ -41,6 +40,79 @@ class ObservationDiscretize(gym.ObservationWrapper):
             bucket_indice.append(bucket_index)
         return tuple(bucket_indice)
 
+# Agente
+class CartpoleDiscreto:
+    def __init__(self, entorno, alpha = 0.5, epsilon = 0.9, gamma = 1):#0.99):
+        self.entorno = entorno
+        self.nEstados = entorno.observation_space.shape[0]
+        self.nAcciones = entorno.action_space.n
+
+        # policy params
+        self.alpha = alpha
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.Q = np.random.uniform(-1, 1, entorno.unwrapped.rangos + (entorno.action_space.n, ))
+        
+        # print(self.Q)
+        self.MIN_ALPHA = 0.1
+        self.MIN_EPSILON = 0.01
+    # end __init__
+    
+    #policy Epsilon-Greedy
+    def seleccionarAccion(self, estado):
+        #exploracion
+        if np.random.rand() <= self.epsilon: #aleatorio
+            return self.entorno.action_space.sample()
+        #explotacion
+        else: # mejor valor Q
+            return np.argmax(self.Q[estado])
+    # end seleccionarAccion
+    
+    def actualizarPolitica(self, estadoAnterior, estadoActual, accion, reward):
+        # q learning
+        best_q = np.amax(self.Q[estadoActual])
+        self.Q[estadoAnterior + (accion, )] += self.alpha * (reward + self.gamma *
+                           best_q - self.Q[estadoAnterior + (accion, )])
+    # end actualizarPolitica
+    
+    def update_explore_rate(self, t):
+        self.epsilon = max(self.MIN_EPSILON, min(self.epsilon, 1.0 - math.log10((t+1)/25)))
+    # end update_explore_rate
+
+    def update_learning_rate(self, t):
+        self.alpha = max(self.MIN_ALPHA, min(self.alpha, 1.0 - math.log10((t+1)/25)))
+    # end update_learning_rate
+    
+    def entrenar(self, episodios, teacherAgent=None, feedbackProbability=0):
+        recompensas = []
+        epsilones = []
+        alphas = []
+
+        for e in range(episodios):
+            estadoAnterior = estadoActual = self.entorno.reset()
+            recompensa = 0
+            fin = False
+
+            while not fin:
+                # self.entorno.render()
+                accion = self.seleccionarAccion(estadoActual)
+                estadoActual, reward, fin, info = self.entorno.step(accion)
+                recompensa += reward
+
+                #actualizar valor Q
+                self.actualizarPolitica(estadoAnterior, estadoActual, accion, reward)
+                estadoAnterior = estadoActual
+
+            print('Fin episodio {}, reward: {}'.format(e, recompensa))
+            recompensas.append(recompensa)
+            epsilones.append(self.epsilon)
+            alphas.append(self.alpha)
+
+            self.update_explore_rate(e)
+            self.update_learning_rate(e)
+
+        return recompensas, epsilones, alphas
+    # end entrenar
 
 # entorno
 cartpole = gym.make("CartPole-v0")
@@ -59,25 +131,13 @@ entorno = gym.wrappers.Monitor(entorno, # entorno cartpole discetizado
                     carpeta, # carpeta de guardado
                     video_callable=False, # grabacion de video
                     force=True) # eliminacion de datos anteriores
-
 # entrenamiento del agente
 episodios = 1000
-t_max = 250 # segun doc gym
-
-for e in range(episodios):
-    estadoActual = entorno.reset()
-
-    for t in range(t_max):
-        # mostrar el escenario
-        # self.entorno.render()
-        # seleccionar accion
-        accion = entorno.action_space.sample()
-        # ejecutar accion
-        estadoActual, reward, fin, info = entorno.step(accion)
-        # fin del escenario
-        if fin:
-            # print("fin episodio t=", t+1, 'r=', reward, 's=', estadoActual)
-            break
+# instanciacion y entrenamiento del agente
+alpha = 0.5
+epsilon = 0.9
+agente = CartpoleDiscreto(entorno, alpha=alpha, epsilon=epsilon)
+agente.entrenar(episodios)
 # fin del monitoreo
 entorno.close()
 # carga de resultados
@@ -85,5 +145,5 @@ results = load_results(carpeta)
 recompensa = results['episode_rewards']
 # graficar
 plt.plot(recompensa)
-plt.legend()
+#plt.legend()
 plt.show()
